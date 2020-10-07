4d3eb9ca495d0afe40a38d84fcbaa6c30d36d946
Dokkeboi <57139237+Dokkeboi@users.noreply.github.com>
2020-10-07 09:56:30 +0200

    Lommeregner W.I.P
    More to be added.

---
+++ Training/sketch.js
@@ -0,0 +1,11 @@
+
+function getResult() {
+    var x = document.getElementById("myBtn1").value;
+    while (document.onclick("myBtn1").value){
+        
+    }
+
+    document.getElementById("myElement1").innerHTML = parseInt(x);
+
+}
+
