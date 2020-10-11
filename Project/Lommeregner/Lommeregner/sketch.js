1bda3f23850308d0840cc3659961b1a905484097
Dokkeboi <57139237+Dokkeboi@users.noreply.github.com>
2020-10-11 17:34:39 +0200

    Done

---
+++ Project/Lommeregner/Lommeregner/sketch.js
@@ -0,0 +1,19 @@
+function myFunction(clickedId) {
+    document.calc.result.value+=clickedId;
+}
+function Clear() {
+    document.calc.result.value="";
+}
+
+/* Metoden at bruge eval() fandt jeg ved nogle søgninger på nettet, eftersom jeg skulle bruge en kortere og nemmere metode.
+At tage et argument, evaluere det og udføre det var den nemmeste løsning for mig */
+function calculate() {
+    try{
+    var inp=eval(document.calc.result.value);
+    document.calc.result.value=inp;
+    }catch(err){
+            document.calc.result.value="Dette input virker ikke :(";
+    }
+}
+
+app.use(express.static('public'));
